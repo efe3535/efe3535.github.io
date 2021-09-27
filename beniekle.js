1c2c15b2e291c9c37c347fb714ac94a38a5f59fb
Ahmet Efe AKYAZI <46342237+efe3535@users.noreply.github.com>
2021-09-27 17:42:54 +0300

    Add files via upload

---
+++ beniekle.js
@@ -0,0 +1,5 @@
+var button = document.getElementById("beni-ekle");
+
+button.addEventListener("click", () => {
+    window.location.href = "https://discord.com/api/oauth2/authorize?client_id=791647205437341736&permissions=309442120774&scope=bot";
+})
