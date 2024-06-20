82198c4f49f66fd70a014978022a4d299f01fe13
A. Efe AKYAZI <a.efe.akyazi@gmail.com>
2024-06-20 21:41:03 +0300

    remove unnecessary log

--- src/app/posts/[id]/page.js
+++ src/app/posts/[id]/page.js
@@ -5,7 +5,6 @@ import Link from "next/link";
 
 const BlogPage = async ({ params }) => {
   const post = await getPostData(params.id)
-  console.log(post)
   return (
     <main className="flex min-h-screen flex-col p-6 bg-background">
       <p className="text-4xl text-foreground self-start">Ahmet Efe Akyazı — Blog</p>
