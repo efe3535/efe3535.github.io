40fb933d132247edf5f8628b47a7529350be923e
A. Efe AKYAZI <a.efe.akyazi@gmail.com>
2024-06-23 00:10:46 +0300

    add underline under my github username

--- src/app/page.js
+++ src/app/page.js
@@ -15,7 +15,7 @@ const Home = () => {
           <p className="text-base self-start text-foreground mt-10">Merhaba, ben Ahmet Efe Akyazı. 16 yaşındayım. Gömülü sistemler, elektronik, mobil uygulamalar ilgi alanlarım arasındadır.
             <br /><br />Genellikle C, Python ve JavaScript ile yazarım.<br />ARM Cortex-M4 serisi ve AVR için kod yazarım. Açık kaynak yazılımlara ilgi duyarım, STM32 için libopencm3&lsquo;ü aktif olarak kullanırım.<br /><br />
             Daha önce, FLL (2020, City Shaper), FRC (2024, Crescendo), Teknofest (2023 Istanbul, 2023 Izmir, 2024 ????) vb. yarışmalara katıldım.
-            <br /><br /><Link href={"https://github.com/efe3535"} className="text-xl text-foreground"><b className="text-2xl">GitHub:</b> efe3535</Link>
+            <br /><br /><Link href={"https://github.com/efe3535"} className="text-xl text-foreground"><b className="text-2xl">GitHub:</b> <span className="underline">efe3535</span></Link>
           </p>
         </div>
         <p className="text-4xl self-start mt-20 underline underline-offset-8">Blog</p>
